ce87989d42c6a3beb30267a975cbc360d50d118b
Saidakbar Ubaydullin <ubayakbar760@gmail.com>
2026-06-12 13:06:22 +0500

    chore: add strict security headers for uzum scraper to bypass bot protection

--- scraper/uzum_scraper.py
+++ scraper/uzum_scraper.py
@@ -13,6 +13,13 @@ class UzumScraper:
             'accept': '*/*',
             'accept-language': 'ru',
             'user-agent': 'Mozilla/5.0 (Linux; Android 15; Pixel 9) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/149.0.0.0 Mobile Safari/537.36',
+            'referer': 'https://www.uzumtezkor.uz/',
+            'sec-ch-ua': '"Google Chrome";v="149", "Chromium";v="149", "Not)A;Brand";v="24"',
+            'sec-ch-ua-mobile': '?1',
+            'sec-ch-ua-platform': '"Android"',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'same-origin'
         })
         
         import os
